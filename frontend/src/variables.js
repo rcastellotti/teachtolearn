cc1db08380ab72fd418586cea105c4b71df23062
Roberto Castellotti <me@rcastellotti.dev>
2022-01-08 23:42:36 +0000

    Update variables.js

--- frontend/src/variables.js
+++ frontend/src/variables.js
@@ -1,8 +1,8 @@
 export const variables = {
     environment: "production",
-    // s3Url: "http://s3.rcastellotti.dev/ttl-dev/",
-    // apiUrl: "https://teachtolearn.club/api/",
-    s3Url: "http://192.168.1.201:9000/ttl-dev/",
-    apiUrl: "http://192.168.1.201:5000/api/",
+    s3Url: "http://s3.rcastellotti.dev/ttl-dev/",
+    apiUrl: "https://teachtolearn.club/api/",
+    // s3Url: "http://192.168.1.201:9000/ttl-dev/",
+    // apiUrl: "http://192.168.1.201:5000/api/",
     mapboxToken: "pk.eyJ1IjoiZmlsaXBwb2dlbnZvYSIsImEiOiJja3dxdGIwd2kwbXJjMm9ydDY4MTM5a3Q4In0.s6u1AJX4NoXuPqyfjFIKZQ"
 };
